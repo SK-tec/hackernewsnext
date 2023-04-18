6dc4d90a5320768171d3f33d251e2e8c7acb9847
samatha <samathagamidi@gmail.com>
2023-04-18 14:37:37 +0200

    key value added to div

--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,10 @@ export default function Home() {
           <div>
             {searchResults.map((news) => {
               return (
-                <div className="block mb-5 rounded-lg bg-white text-blue-700 text-center shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+                <div
+                  key={news.objectID}
+                  className="block mb-5 rounded-lg bg-white text-blue-700 text-center shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700"
+                >
                   <div className="flex justify-between border-b-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
                     <div>Author: {news.author}</div>
                     <div>
